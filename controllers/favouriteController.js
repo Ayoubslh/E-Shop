e216cb51b1f96367f2aa451631feec8fb04c5e72
Ayoubslh <ayoub801@gmail.com>
2025-08-20 23:02:06 +0200

    FIXED  favs works in postman

--- controllers/favouriteController.js
+++ controllers/favouriteController.js
@@ -1,17 +1,15 @@
 const Favourite = require("../models/Favourite");   
 const Item = require("../models/Item");
+const AppError = require('./../utils/appError');
 
 exports.getFavourites = async (req, res, next) => {
     try {
         const favourites = await Favourite.findOne({ user: req.user.id })
             .populate({
                 path: "items",
-                populate: {
-                    path: "product",
-                    select: "name brand image price"
-                }
-            })
-            
+                select: "name brand image price"
+            });
+
         if (!favourites) return next(new AppError('No favourites found', 404));
         res.status(200).json({
             status: 'success',
@@ -26,15 +24,19 @@ exports.getFavourites = async (req, res, next) => {
 
 exports.addToFavourites = async (req, res, next) => {
     try {
-        const userId = req.user.id;
+        const userId = req.user.id; 
         if (!userId) return next(new AppError("You are not authorized", 403));
 
-        const itemId = req.body.itemId;
-        if (!itemId) return next(new AppError("Item ID is required", 400));
+        const itemIds = req.body.items;
+        if (!itemIds || !Array.isArray(itemIds)) {
+            return next(new AppError("Item IDDs are required", 400));
+        }
 
         // Check if the item exists
-        const item = await Item.findById(itemId);
-        if (!item) return next(new AppError(`Item with ID ${itemId} not found`, 404));
+        const items = await Item.find({ _id: { $in: itemIds } });
+        if (!items || items.length === 0) {
+            return next(new AppError(`Items not found`, 404));
+        }
 
         // Find or create user's favourites
         let favourites = await Favourite.findOne({ user: userId });
@@ -46,10 +48,12 @@ exports.addToFavourites = async (req, res, next) => {
         }
 
         // Add item to favourites if not already present
-        if (!favourites.items.includes(itemId)) {
-            favourites.items.push(itemId);
-            await favourites.save();
-        }
+        items.forEach(async (item) => {
+            if (!favourites.items.includes(item._id)) {
+                favourites.items.push(item._id);
+            }
+        });
+        await favourites.save();
 
         res.status(200).json({
             status: "success",
